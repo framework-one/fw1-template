954c5f9d78b081cdd885dd4962d5f369fa4d29f4
Sean Corfield <sean@corfield.org>
2013-08-19 17:32:10 -0700

    Update to FW/1 0.2.0

--- src/leiningen/new/fw1/project.clj
+++ src/leiningen/new/fw1/project.clj
@@ -1,5 +1,5 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.5.1"]
-                           [framework-one "0.2.0-SNAPSHOT"]]
+                           [framework-one "0.2.0"]]
             :main {{name}}.main)
