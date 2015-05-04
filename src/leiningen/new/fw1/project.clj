345f0ea342768efc474b6f43bdc0df7f7ff27fb1
Sean Corfield <sean@worldsingles.com>
2015-05-03 19:49:47 -0700

    Bump to 0.4.0 and 2015 etc

--- src/leiningen/new/fw1/project.clj
+++ src/leiningen/new/fw1/project.clj
@@ -1,5 +1,5 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.5.1"]
-                           [framework-one "0.3.3"]]
+            :dependencies [[org.clojure/clojure "1.6.0"]
+                           [framework-one "0.4.0"]]
             :main {{name}}.main)
